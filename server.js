8860a19f3f5f3fb9078ead5ea3d51fe5093e35c6
John Munger <johntmunger@gmail.com>
2020-07-13 14:32:14 -0600

    chore: Send html page to the browser

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const http = require('http');
-const server = http.createServer((req, server) => consolse.log('Request made'));
+const fs = require('fs');
+
+const server = http.createServer((req, res) => { 
+  console.log('Request made', req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-type', 'text/html');
+
+  // send html
+  fs.readFile('./views/index.html', (err, data) => {
+    if(err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
+
+});
 
 server.listen(3000, 'localhost', () => { 
   console.log('Server is listening on port 3000')
