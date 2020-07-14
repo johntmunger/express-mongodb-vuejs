f39e6cfe3c84d67e0d61daa32f4b3c4fa9ac892a
John Munger <johntmunger@gmail.com>
2020-07-14 11:22:55 -0600

    chore: Set up the mongodb connection and get request to initialize an empty array

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,13 +4,22 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // get posts
-router.get('/', (req, res) => {
-  res.send('hello');
-})
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+});
 
 // add posts
 
 // delete posts
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect('mongodb+srv://adminabcd:1234@fullstackapp.jnuvp.mongodb.net/fullstackApp?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+  });
+
+  return client.db('fullstack-app').collection('posts');
+}
 
 
 module.exports = router;
