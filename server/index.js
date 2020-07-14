8214a47957f0a560012f4b637886d09c494b8a4f
John Munger <johntmunger@gmail.com>
2020-07-14 10:52:00 -0600

    chore: Build a new route for posts versus blogs and git rid of mongoose

--- server/index.js
+++ server/index.js
@@ -9,27 +9,32 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = require('./routes/api/posts');
+app.use('/api/posts', posts);
+
 const port = process.env.PORT || 3000;
 
 // listen for requests
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
-app.get('/', (req, res) => {
-  // res.send('<p>home page</p>'); // plain text
-  res.sendFile('./views/index.html', { root: __dirname });
-});
 
-app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>'); // plain text
-  res.sendFile('./views/about.html', { root: __dirname });
-});
+// //////////////////////////////////////////
+// BELOW IS FROM ANOTHER SET OF ROUTES
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile('./views/index.html', { root: __dirname });
+// });
 
-// 404 page
-app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname })
-})
+// app.get('/about', (req, res) => {
+//   res.sendFile('./views/about.html', { root: __dirname });
+// });
+
+// // redirects
+// app.get('/about-us', (req, res) => {
+//   res.redirect('/about');
+// });
+
+// // 404 page
+// app.use((req, res) => {
+//   res.status(404).sendFile('./views/404.html', { root: __dirname })
+// });
